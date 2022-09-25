8511ad19f5f8d2854a3f167994704fdf31c160cb
El Migue <miguel.bennett.valencia@utelvt.edu.ec.git config --global user.name El>
2022-09-25 01:29:51 -0500

    modificacion de velocimetro

--- velocimetro.cpp
+++ velocimetro.cpp
@@ -1,12 +1,13 @@
-//Creador:Mayerli Arias
+//Modificado por:Mayerli Anahi Arias Tenorio 
 #include <iostream>
 using namespace std;
 int main ()
 {
-        float v,d,t;
-        cout<<"Ingrese la distancia(en km) d=: ";cin>>d;
-        cout<<"Ingrese el tiempo(en horas) t=: ";cin>>t;
-        v=d/t;
-        cout<<"La velocidad calculada es de v= "<<v<<endl<<endl;
+        float M,A,T;
+        cout<<"Ingrese la distancia(en km) A=: ";cin>>A;
+        cout<<"Ingrese el tiempo(en horas) T=: ";cin>>T;
+        M=A/T;
+        cout<<"La velocidad calculada es de M= "<<M<<endl<<endl;
         return 0;
+
 }
